5476eead5fec9469bae968d5fde4c42ae53688b4
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-08-27 21:32:26 +0700

    53 - Add validation dependency and implement on functions

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.springboot.entity.User;
 import com.example.springboot.exception.ErrorDetails;
 import com.example.springboot.exception.ResourceNotFoundException;
 import com.example.springboot.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class UserController {
 
     // build create User REST API
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user) {
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user) {
         UserDto savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
@@ -48,6 +49,8 @@ public class UserController {
     public ResponseEntity<UserDto> updateUser(
             @PathVariable("id")
             Long userId,
+
+            @Valid
             @RequestBody
             UserDto user
     ) {
--- src/main/java/com/example/springboot/dto/UserDto.java
+++ src/main/java/com/example/springboot/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.example.springboot.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +14,15 @@ import lombok.Setter;
 public class UserDto {
 
     private Long id;
+
+    @NotEmpty(message = "First name is required")
     private String firstName;
+
+    @NotEmpty(message = "Last name is required")
     private String lastName;
+
+    @NotEmpty(message = "Email is required")
+    @Email(message = "Email is invalid")
     private String email;
 
 }
