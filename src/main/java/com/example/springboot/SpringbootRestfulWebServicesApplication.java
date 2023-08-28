6c654f7aa11e69b6b340fbae0fca6e61e7d0b4af
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-08-28 19:11:52 +0700

    70 - Define General API Information using Annotations

--- src/main/java/com/example/springboot/SpringbootRestfulWebServicesApplication.java
+++ src/main/java/com/example/springboot/SpringbootRestfulWebServicesApplication.java
@@ -1,11 +1,36 @@
 package com.example.springboot;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API Documentation",
+				description = "Spring Boot REST API Documentation using springdoc-openapi and OpenAPI 3.0",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Matthew Alfredo",
+						email = "matthewwalfredoo@gmail.com",
+						url = "https://github.com/matthewalfredoo"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/matthewalfredoo"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Management Documentation",
+				url = "https://github.com/matthewalfredoo"
+		)
+)
 public class SpringbootRestfulWebServicesApplication {
 
 	@Bean
